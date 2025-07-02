07361a36f021edb799618120974bd48ea593d89f
sowmiyav24 <sowmiyav1804@gmail.com>
2025-07-02 14:57:02 +0530

    marks

---
+++ src/com/vetias/java/Marks.java
@@ -0,0 +1,9 @@
+public class Marks{
+
+    public static void main(String[]args){
+        int[] Marks={97,92,93,94,95};
+        for(int mark:Marks){
+            System.out.println(mark);
+        }
+}
+}
