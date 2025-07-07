5915e7812ed1d4430fe762ac01c7da02ca80859b
sowmiyav24 <sowmiyav1804@gmail.com>
2025-07-07 15:59:49 +0530

    scanner

---
+++ src/com/vetias/java/workshop/basics/Array/NameGenerator.java
@@ -0,0 +1,29 @@
+package workshop.basics.Array;
+
+import java.util.Scanner;
+
+public class NameGenerator {
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name= scanner.nextLine();
+        System.out.println("Enter your gender:");
+        String gender= scanner.nextLine();
+        System.out.println("Enter your qualification:");
+        String qualification= scanner.nextLine();
+        if("male".equals(gender)){
+            System.out.println("Mr");
+        }
+        else if("female".equals(gender)){
+            System.out.println("Miss");
+        }
+        else{
+            System.out.println("hello");
+        }
+        scanner.close();
+
+        }
+        
+
+    }
--- src/com/vetias/java/workshop/basics/Array/StringBuilderExample.java
+++ src/com/vetias/java/workshop/basics/Array/StringBuilderExample.java
@@ -7,9 +7,7 @@ public class StringBuilderExample {
         sb.append("Java").append(" ").append("is").append(" ").append("the").append(" ").append("most").append(" ").append("used").append(" ").append("language").append(" ").append("for").append(" ").append("enterprise").append(" ").append("Applications");
         System.out.println(sb.toString());  
         
-        // sb.append("soum").append("ya");
-        // System.out.println(sb.toString());
-
+        
 
     }
     }
