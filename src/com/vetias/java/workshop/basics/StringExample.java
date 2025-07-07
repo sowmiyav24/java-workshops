1d4cf798b51b2b26bbf3ef531cda76936dc944c8
sowmiyav24 <sowmiyav1804@gmail.com>
2025-07-07 12:10:33 +0530

    stringcount

--- src/com/vetias/java/workshop/basics/StringExample.java
+++ src/com/vetias/java/workshop/basics/StringExample.java
@@ -12,18 +12,31 @@
 // }
 
 
+// package workshop.basics;
+
+// public class StringExample {
+
+//     public static void main(String[] args) {
+//         String sentence="This program shows how can we split a string into multiple strings";
+//         String[] words = sentence.split("\\s+");
+
+//         System.out.println("Words in the sentence:");
+//         for (String word : words) {
+//             System.out.println(word);
+//         }
+//     }
+// }
+    
 package workshop.basics;
 
 public class StringExample {
 
     public static void main(String[] args) {
-        String sentence="This program shows how can we split a string into multiple strings";
-        String[] words = sentence.split("\\s+");
-
-        System.out.println("Words in the sentence:");
-        for (String word : words) {
-            System.out.println(word);
-        }
+        String text="This program shows how can we split a"+ "string into multiple strings. We need to find sentence," +"words and letters count";
+        String[] sentences = text.split("\\.");
+        String[] words = text.split(" ");
+        System.out.println("sentence:" + sentences.length);
+        System.out.println("Words:" + words.length);
+        System.out.println("Characters" + text.length());
     }
 }
-    
