2af06d8e62f0c45639aa430677302baa0ef7eb1a
sowmiyav24 <sowmiyav1804@gmail.com>
2025-07-08 11:10:45 +0530

    Timeleft

--- src/com/vetias/java/workshop/basics/Array/AgeCalculator.java
+++ src/com/vetias/java/workshop/basics/Array/AgeCalculator.java
@@ -1,17 +1,32 @@
+// package workshop.basics.Array;
+
+// import java.util.Scanner;
+
+// public class AgeCalculator {
+//     public static void main(String[] args) {
+        
+//     // Scanner scanner=new Scanner(System.in);
+//     // System.out.println("Enter your birthofyear:");
+//     // int birthofyear =scanner.nextInt();
+//     // int currentyear=2025;
+//     // int age=currentyear-birthofyear;
+//     // System.out.println("your age:"+age );
+
+// }
+// }
+
+
 package workshop.basics.Array;
 
+import java.time.LocalDate;
 import java.util.Scanner;
 
-public class AgeCalculator {
+public class AgeCalculator{
     public static void main(String[] args) {
-        
-    Scanner scanner=new Scanner(System.in);
-    System.out.println("Enter your birthofyear:");
-    int birthofyear =scanner.nextInt();
-    int currentyear=2025;
-    int age=birthofyear-currentyear;
-    System.out.println("your age"+age );
-
-}
-}
-
+        System.out.println("Enter your Date of Birth:(YYYY-MM-DD)");
+        String birthofyear = input Scanner.nextLine();
+        LocalDate birthdate= LocalDate.parse(birthofyear);
+        int birthdate=birthdate.getYear();
+    int currentyear=LocalDate.now (). getyear();
+    }
+}  
