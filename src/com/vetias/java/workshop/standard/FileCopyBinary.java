1d87681d163699b8535417217fab10bf72b25b62
sowmiyav24 <sowmiyav1804@gmail.com>
2025-07-09 11:35:38 +0530

    Filecopybinary

---
+++ src/com/vetias/java/workshop/standard/FileCopyBinary.java
@@ -0,0 +1,24 @@
+package workshop.standard;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileCopyBinary {
+    public static void main(String[] args) {
+        try(FileInputStream inputStream=new FileInputStream("foodie.jpg");
+            FileOutputStream outputStream=new FileOutputStream("foodie.jpg"))
+            {
+                byte[] chunk = new byte[1024];
+                int byteRead;
+                while ((byteRead=inputStream.read(chunk)) !=-1) {
+                        outputStream.write(chunk,0,byteRead);
+
+                }         
+            } catch (IOException e) {
+                System.out.println("Error in reading or writing the file");
+                
+            } 
+    }
+
+}  
