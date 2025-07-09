10484db5227c2ed8b21d363df13309ff4b4c36be
sowmiyav24 <sowmiyav1804@gmail.com>
2025-07-09 10:15:50 +0530

    characterbasedexample

---
+++ src/com/vetias/java/workshop/standard/CharacterBasedExample.java
@@ -0,0 +1,22 @@
+package workshop.standard;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CharacterBasedExample {
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("README.md"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            
+        } catch (IOException e) {
+            System.out.println("Error Reading file..");
+            e.printStackTrace();
+        }
+    }
+}
+
+
