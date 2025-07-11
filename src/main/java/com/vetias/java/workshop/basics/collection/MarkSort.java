45958089efb30b123d892dc53f4cc4bc1c32b34a
sowmiyav24 <sowmiyav1804@gmail.com>
2025-07-11 09:54:56 +0530

    set

---
+++ src/main/java/com/vetias/java/workshop/basics/collection/MarkSort.java
@@ -0,0 +1,14 @@
+package com.vetias.java.workshop.basics.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MarkSort {
+    public static void main(String[] args) {
+        List<Integer>marks= new ArrayList();
+        marks.add=()
+
+    
+    }
+
+}
--- src/main/java/com/vetias/java/workshop/basics/collection/StudentMap.java
+++ src/main/java/com/vetias/java/workshop/basics/collection/StudentMap.java
@@ -16,9 +16,6 @@ public class StudentMap {
         System.out.println(students);
         System.out.println(students.keySet());
         System.out.println(students.values());
-        
-        
-        
     }
 
 }
